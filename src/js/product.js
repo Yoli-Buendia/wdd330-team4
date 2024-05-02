48967c91cbb1c5a1b8f1fe8232810af53ed321de
Adam <tttgkm@gmail.com>
2024-05-02 12:26:17 -0700

    init branch

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,14 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartStor = getLocalStorage("so-cart");
+  let cart = cartStor != null ? cartStor : [];
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+
+  // setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
