e191efac4136e77b4b53cbca3fe49fcb425b10a0
Yoli-Buendia <bue17003@byui.edu>
2024-05-13 23:34:48 -0600

    first attempt of Add a visual indicator of how much a product is discounted to the product listing page.

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -7,6 +7,7 @@ export default async function productDetails(productId) {
     const product = await findProductById(productId);
 
     renderProductDetails(product);
+    discountHome(product);
 }
 
 function addProductToCart(product) {
@@ -21,27 +22,50 @@ function addProductToCart(product) {
   }
 
 function renderProductDetails(product) {
-    document.getElementById("productName").innerText = product.Name;
-    document.getElementById("productNameWithoutBrand").innerText = product.NameWithoutBrand;
-    document.getElementById("productImage").setAttribute("src", product.Image);
-    document.getElementById("productImage").setAttribute("alt", product.NameWithoutBrand);
+  document.getElementById("productName").innerText = product.Name;
+  document.getElementById("productNameWithoutBrand").innerText = product.NameWithoutBrand;
+  document.getElementById("productImage").setAttribute("src", product.Image);
+  document.getElementById("productImage").setAttribute("alt", product.NameWithoutBrand);
 
-    if(product.FinalPrice < product.SuggestedRetailPrice){
-      //Consider putting this in Utility
-      const currency = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD',});
-      
-      const discountAmount = Math.round(product.SuggestedRetailPrice - product.FinalPrice)
-      document.getElementById("productRetailPrice").innerText = `Retail Price: ${currency.format(product.SuggestedRetailPrice)}`;
-      document.getElementById("productDiscountAmount").innerText = `Discount: ${currency.format(discountAmount)}`;
-    }
+  if(product.FinalPrice < product.SuggestedRetailPrice){
+    //Consider putting this in Utility
+    const currency = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD',});
+    
+    const discountAmount = Math.round(product.SuggestedRetailPrice - product.FinalPrice)
+    document.getElementById("productRetailPrice").innerText = `Retail Price: ${currency.format(product.SuggestedRetailPrice)}`;
+    document.getElementById("productDiscountAmount").innerText = `Discount: ${currency.format(discountAmount)}`;
+  }
 
-    document.getElementById("productFinalPrice").innerText = `Sale Price: $${product.FinalPrice}`;
-    document.getElementById("productColorName").innerText = product.Colors[0].ColorName;
-    document.getElementById("productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
-    document.getElementById("addToCart").setAttribute("data-id", product.Id);
+  document.getElementById("productFinalPrice").innerText = `Sale Price: $${product.FinalPrice}`;
+  document.getElementById("productColorName").innerText = product.Colors[0].ColorName;
+  document.getElementById("productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+  document.getElementById("addToCart").setAttribute("data-id", product.Id);
 
-    document
+  document
 
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+.getElementById("addToCart")
+.addEventListener("click", addToCartHandler);
+}
+
+function discountHome (product){
+  if(product.FinalPrice < product.SuggestedRetailPrice){
+    //Consider putting this in Utility
+    const currency = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD',});
+    
+    const discountAmount = Math.round(product.SuggestedRetailPrice - product.FinalPrice)
+    document.getElementById("productRetailPrice").innerText = `Retail Price: ${currency.format(product.SuggestedRetailPrice)}`;
+
+    
+    document.getElementById("productDiscountAmount").innerText = `Discount: ${currency.format(discountAmount)}`;
+    
+    //id home
+    document.getElementById("productDiscountAmount-home1").innerText = `Discount: ${currency.format(discountAmount)}`;
+    /*document.getElementById("productDiscountAmount-home2").innerText = `Discount: ${currency.format(discountAmount)}`;
+    document.getElementById("productDiscountAmount-home3").innerText = `Discount: ${currency.format(discountAmount)}`;
+    document.getElementById("productDiscountAmount-home4").innerText = `Discount: ${currency.format(discountAmount)}`;*/
+    
+    document.getElementsByClassName("product-card__discount-home").innerText = `Discount: ${currency.format(discountAmount)}`;
+   // console.log(document.getElementById("productDiscountAmount-home1"));
+
+  }
 }
